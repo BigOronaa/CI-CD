17b05f627db02c3f41588f1618b7864b11dae516
Igogo Great Oro-Oghene <edwardoronaa@gmail.com>
2025-12-04 11:21:25 -0800

    Update Node version to 20 for CI workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    ignores: ["node_modules/**"],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,   // allow console, process, require, module
+        ...globals.jest,   // allow test, expect, describe
+      },
+    },
+  },
+];
